af0208c6b74416e1c3dcfcc3ed88d0944c925fd3
CarYip <111323125+CarYipTC@users.noreply.github.com>
2026-02-06 17:05:24 +0000

    Create DataWarehouse database and schemas
    This script creates a new database named 'DataWarehouse' and defines three schemas: bronze, silver, and gold.

---
+++ script/init_database.sql
@@ -0,0 +1,25 @@
+/*
+=============================
+Create Database and Schemas
+==============================
+Script purpose:
+This script creat
+
+
+
+
+
+*/
+use master;
+go
+
+create database DataWarehouse;
+Go
+
+use DataWarehouse;
+Go
+create schema bronze;
+Go
+create schema silver;
+Go
+create schema gold;
